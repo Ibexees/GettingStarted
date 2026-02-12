eed4f1978d30dc1c7743ebe8ae384365d83c8a77
Marius <marius.marik.19@gmail.com>
2026-02-12 10:18:35 +0100

    first Commit

---
+++ Assets/Script/cubeRotation.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class cubeRotation : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UnityEngine.Debug.Log("HelloWorld");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
